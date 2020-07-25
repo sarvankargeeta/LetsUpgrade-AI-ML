7c3ff47bcb81e5db8d5935aa5271fd9643dd0027
sarvankargeeta <45433060+sarvankargeeta@users.noreply.github.com>
2020-07-25 16:27:58 +0300

    Add files via upload

---
+++ Day 7-Day13/Attrition Assignment.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 23 12:33:49 2020
+
+@author: Geeta Sarvankar
+"""
+
+import pandas as pd
+
+import numpy as np
+
+import matplotlib.pyplot as plot
+
+dataset1 = pd.read_excel('E:\Rutuja\LockDown\AI and ML\Day 7-Notes and assignments\Assignment\general_data.xlsx', sheet_name=0)
+
+dataset1.head()
